d51d930088a2157fe7b88f6bd8a6d9f44e321428
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-08-16 14:49:52 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 253 ms, Memory: 62,744 KB -BaekjoonHub

--- SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
@@ -1,14 +1,14 @@
 # 의석이의 세로로 말해요
 T = int(input())
 for tc in range(1,T+1):
-    M = [[""]*18 for _ in range(18)] # 가로 세로를 넉넉하게 만들어주기
+    M = [[""]*16 for _ in range(16)] # 가로 세로를 넉넉하게 만들어주기
     A = [list(input()) for _ in range(5)]
     for i in range(5):
         for j in range(len(A[i])):
             M[i][j]+=A[i][j] # M이라는 새로운 공간에 A를 넣어줌
 
     print(f'#{tc}', end=" ")
-    for k in range(18) :
-        for h in range(18):
+    for k in range(16) :
+        for h in range(16):
             print(M[h][k], end="")
     print()
