7a298b7d42584b6f864b17930180941032eb6c9b
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-08-24 22:25:46 +0900

    [Bronze II] Title: 방 배정, Time: 124 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,18 @@
+#백준 13300 방배정
+N, K = map(int,input().split())
+arr = [[0]*2 for _ in range(7)]
+for _ in range(N):
+    S,Y = map(int,input().split()) # S : 성별 남:1 여:0, Y: 학년
+    arr[Y][S]+=1
+
+sum = 0
+for i in range(7):
+    for j in range(2):
+        if arr[i][j] !=0:
+            if (arr[i][j] % K==0):
+                sum += int(arr[i][j]/K)
+            else :
+                sum+=((arr[i][j]//K)+1)
+
+
+print(sum)
