3adbb516c4a6a9383bd39e2afcd4eaab8b17f159
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-08-24 22:41:08 +0900

    [Bronze II] Title: 방 배정, Time: 108 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -1,4 +1,3 @@
-#백준 13300 방배정
 N, K = map(int,input().split())
 arr = [[0]*2 for _ in range(7)]
 for _ in range(N):
@@ -9,10 +8,13 @@ sum = 0
 for i in range(7):
     for j in range(2):
         if arr[i][j] !=0:
-            if (arr[i][j] % K==0):
-                sum += int(arr[i][j]/K)
+            if arr[i][j] <= K:
+                sum += 1
             else :
-                sum+=((arr[i][j]//K)+1)
+                if arr[i][j] % K == 0:
+                    sum+=(arr[i][j]//K)
+                else:
+                    sum+=((arr[i][j]//K)+1)
 
 
 print(sum)
