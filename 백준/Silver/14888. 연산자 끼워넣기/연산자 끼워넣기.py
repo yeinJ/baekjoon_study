35957fcfeee08c5512c457bb806b46451a1323f0
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-09-25 18:45:05 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 104 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,33 +1,32 @@
-from itertools import permutations
-N = int(input())
-num = list(map(int,input().split()))
-ope = list(map(int,input().split()))
-# 0:+, 1:-, 2:*, 3://
-opera = []
-for i in range(4):
-    for j in range(ope[i]):     # 해당하는 oper값 개수만큼 opera에 넣어주기
-        opera.append(i)
-operations=list(permutations(opera,sum(ope)))       # 연산자를 통해 순열 생성
+n = int(input())
+data = list(map(int,input().split()))
+add,sub,mul,div = map(int,input().split())
 
-max_ans = int(-1e11)
-min_ans = int(1e11)      # 10억 이상의 큰 수로 지정
-for i in operations:
-    ans = num[0]
-    for j in range(1,N):
-        if i[j-1]==0:
-            ans =ans + num[j]
-        elif i[j-1]==1:
-            ans = ans-num[j]
-        elif i[j-1]==2:
-            ans = ans*num[j]
-        else :
-            if ans < 0 :
-                ans=-(ans)//num[j]
-                ans = -ans
-            else :
-                ans = ans//num[j]
-    max_ans = max(max_ans,ans)
-    min_ans = min(min_ans,ans)
+min_value = 1e9
+max_value = -1e9
 
-print(max_ans)
-print(min_ans)
+def dfs(i,now):
+    global min_value,max_value,add,sub,mul,div
+    if i == n :
+        min_value = min(min_value,now)
+        max_value = max(max_value,now)
+    else:
+        if add>0:
+            add-=1
+            dfs(i+1,now+data[i])
+            add+=1
+        if sub>0:
+            sub-=1
+            dfs(i+1,now-data[i])
+            sub+=1
+        if mul>0:
+            mul-=1
+            dfs(i+1,now*data[i])
+            mul+=1
+        if div>0:
+            div-=1
+            dfs(i+1,int(now/data[i]))
+            div+=1
+dfs(1,data[0])
+print(max_value)
+print(min_value)
