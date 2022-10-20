690362961c1e45672e91131a07cbdb55be767115
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-10-20 15:41:10 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString();
+input = parseInt(input);
+
+if(input>=90){
+  console.log('A')
+}
+else if(input>=80){
+  console.log('B')
+}
+else if(input>=70){
+  console.log('C')
+}
+else if(input>=60){
+  console.log('D')
+}
+else{
+  console.log('F')
+}
