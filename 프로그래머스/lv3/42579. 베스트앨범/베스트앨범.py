d1df37deb9145a978499feb3b3f6d89d5506f2a0
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-11-05 20:42:57 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,36 @@
+'''
+속한 노래가 많이 재생된 장르 먼저 수록
+장르 내에 많이 재생된 노래 먼저 수록
+재생횟수가 같으면 고유번호 낮은 노래 먼저 수록
+i는 고유번호가 i인 노래 장르
+'''
+def solution(genres, plays):
+    n = len(genres)
+    answer=[]
+    gen = list(set(genres))
+    dic_gen = dict()
+    for i in range(n):
+        if genres[i] in dic_gen:
+            dic_gen[genres[i]].append(plays[i])
+        else :
+            dic_gen[genres[i]]=[plays[i]]
+        dic_gen[genres[i]].sort(reverse=True)
+    sorted_dic = sorted(dic_gen.items(), key=lambda item:sum(item[1]), reverse=True)
+    # for i in sorted_dic:
+    #     i[1]
+    for i in sorted_dic:
+        cnt = 0
+        for h in range(len(i[1])):
+            for j in range(n):
+                if i[1][h]==plays[j] and i[0]==genres[j]:
+                    if j not in answer:
+                        answer.append(j)
+                        cnt +=1
+                        break
+                    else :
+                        continue
+                    
+            if cnt == 2:
+                break
+    
+    return answer
