53db251517581a1d25043df63b4256be9ca54c24
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-11-05 17:10:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.90 ms, Memory: 35.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,14 @@
+def solution(participant, completion):
+    answer=[]
+    list_par=list(set(participant))
+    list_num=[0 for _ in range(len(list_par))]
+    dict_par=dict(zip(list_par,list_num))
+    for i in participant:
+        dict_par[i]+=1
+    for i in completion:
+        dict_par[i]-=1
+    for key,value in dict_par.items():
+        if value!=0:
+            answer.append(key)
+            
+    return ''.join(answer)
