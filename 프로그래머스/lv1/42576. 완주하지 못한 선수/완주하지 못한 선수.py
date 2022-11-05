34c6e01d45691f2c9fe765cc8d1ba31f825d5373
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-11-05 17:12:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.63 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,19 @@
+import collections
+
 def solution(participant, completion):
-    answer=[]
-    list_par=list(set(participant))
-    list_num=[0 for _ in range(len(list_par))]
-    dict_par=dict(zip(list_par,list_num))
-    for i in participant:
-        dict_par[i]+=1
-    for i in completion:
-        dict_par[i]-=1
-    for key,value in dict_par.items():
-        if value!=0:
-            answer.append(key)
+#     answer=[]
+#     list_par=list(set(participant))
+#     list_num=[0 for _ in range(len(list_par))]
+#     dict_par=dict(zip(list_par,list_num))
+#     for i in participant:
+#         dict_par[i]+=1
+#     for i in completion:
+#         dict_par[i]-=1
+#     for key,value in dict_par.items():
+#         if value!=0:
+#             answer.append(key)
             
-    return ''.join(answer)
+#     return ''.join(answer)
+
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer)[0]
