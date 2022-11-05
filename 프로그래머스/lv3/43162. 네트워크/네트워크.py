deaa042814190c6625bcdcb766f984cde2bb8c48
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-11-05 23:03:46 +0900

    [level 3] Title: 네트워크, Time: 0.59 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,18 +1,22 @@
+from collections import deque
 def solution(n, computers):
-    visited = []
-    intranet = []
-    network = []
-    def connect(node):
-        visited.append(node)
-        intranet.append(node)
-        for nextnode in range(n):
-            if nextnode not in visited and computers[node][nextnode]==1:
-                connect(nextnode)
-        return intranet
-    
-    for node in range(n):
-        if node not in visited:
-            network.append(connect(node))
-            intranet = []
-    return len(network)
-                
+    visited = [0]*n
+    answer = 0
+    def dfs(i):
+        nonlocal visited
+        network = deque()
+        network.append(i)
+        visited[i]=1
+        while network:
+            si=network.popleft()
+            for j in range(n):
+                if (computers[si][j]==1 or computers[j][si]==1) and si!=j and visited[j]==0:
+                    visited[j]=1
+                    network.append(j)
+                    
+    for i in range(n):
+        if visited[i]!=1:
+            dfs(i)
+            answer +=1
+            
+    return answer
