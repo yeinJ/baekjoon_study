3f90379582d980e082ca7c1113da4ea95fe3d6de
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-11-13 12:28:57 +0900

    [level 2] Title: 타겟 넘버, Time: 6.28 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,15 @@
+from collections import deque
 def solution(numbers, target):
-    
-    def find_answer(sum_num,i):
-        nonlocal count
-        if sum_num == target and i == len(numbers):
-            count +=1
-            return
-        elif i==len(numbers):
-            return    
-        else :
-            k=numbers[i]
-            find_answer(sum_num+k,i+1)
-            find_answer(sum_num-k,i+1)
+    queue = deque()
+    queue.append([numbers[0],0])
+    queue.append([-numbers[0],0])
+    cnt = 0
+    while queue :
+        v,i = queue.popleft()
+        if i==len(numbers)-1 and v == target:
+            cnt +=1
+        elif i < len(numbers)-1:
+            queue.append([v+numbers[i+1],i+1])
+            queue.append([v-numbers[i+1],i+1])
 
-             
-    count = 0
-    find_answer(0,0)
-    
-    return count
+    return cnt
