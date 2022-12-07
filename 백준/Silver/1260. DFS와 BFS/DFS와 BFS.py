9768ac799eeded45ccf34560c4e84ac02ce9d3b3
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-12-07 13:17:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 456 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,38 +1,46 @@
-import sys
-sys.setrecursionlimit(10**5)
-from collections import deque
+'''
+DFS로 탐색한 결과
+BFS로 탐색한 결과 출력
+'''
 
+n,m,v = map(int,input().split()) # n:정점의 개수, m:간선의 개수, v: 탐색 시작할 번호
+node=[[] for _ in range(n+1)]
+
+
+for _ in range(m):
+    a,b = map(int,input().split()) # 간선이 연결하는 두 번호 - 양방향
+    node[a].append(b)
+    node[b].append(a)
+visited=[False]*(n+1)
+dfs_list = []
+for i in node:
+    i.sort()
+
+
+# dfs
 def dfs(v):
+    global dfs_list
     visited[v]=True
-    print(v, end=' ')
-    for i in graph[v]:
+    dfs_list.append(v)
+    for i in node[v]:
         if not visited[i]:
             dfs(i)
-
-def bfs(v):
-    # 큐 구현을 위해 deque 라이브러리 사용
-    queue = deque([v])
-    visited[v]=False
-    while queue:
-        s=queue.popleft()
-        print(s, end=' ')
-        for i in graph[s]:
-            if visited[i]:
-                queue.append(i)
-                visited[i]=False
-
-
-n,m,v = map(int,input().split())
-# N: 정점의 개수, M: 간선의 개수, V:탐색시작할 정점의 번호
-graph = [[] for _ in range(10001)]
-visited = [False]*(10001)
-for _ in range(m):
-    a,b=map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-for i in range(m+1):
-    if graph[i]:
-        graph[i].sort()     # 작은 번호부터 찾아야 하기에 graph 정렬해줌
 dfs(v)
-print()
-bfs(v)
+
+# bfs
+bfs_list = [v]
+from collections import deque
+q=deque()
+q.append(v)
+visited[v]=False
+while q:
+    start=q.popleft()
+    for i in node[start]:
+        if visited[i]:
+            bfs_list.append(i)
+            visited[i] = False
+            q.append(i)
+
+
+print(*dfs_list)
+print(*bfs_list)
