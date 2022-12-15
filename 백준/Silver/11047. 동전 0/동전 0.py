27bf78f26e99c78ca6c24a06aa8d946d9b883e79
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2022-12-16 01:27:13 +0900

    [Silver IV] Title: 동전 0, Time: 56 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,13 +1,20 @@
-N,K = map(int,input().split())
-money = []
-for _ in range(N):
-    Ai=int(input())
-    money.append(Ai)
+'''
+준규가 가지고 있는 동전은 N종류
+각각의 동전을 매우 많이 가지고 있음
+동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 함
+이때 필요한 동전 개수의 최솟값 구하기
+'''
 
-give=0
-for i in sorted(money,reverse=True):
-    if K//i >= 1:
-        give += (int(K//i))
-        K%=i
+import sys
+input = sys.stdin.readline
+N,K = map(int,input().split()) # N : 동전의 종류 K:가치의 합
+Ai = [int(input()) for _ in range(N)] # 동전의 가치
+count = 0
 
-print(give)
+for i in range(len(Ai)-1,-1,-1):
+    if K//Ai[i]>=1:
+        count+=(K//Ai[i]) # 동전의 최댓값을 이용해 몫을 구하고 해당 몫을 count에 더해준다
+        # 그리고 나머지를 K로 갱신해준 후, K가 0이 될 때까지 해당 과정을 반복해준다.
+        K%=Ai[i]
+        
+print(count)
