a28fddc8ff0f68ce06012b00abb781b05b44158d
YeinJeong <109320718+yeinJ@users.noreply.github.com>
2023-03-06 22:13:42 +0900

    [level 2] Title: 타겟 넘버, Time: 6.35 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,23 @@
+'''
+n개의 음이 아닌 정수
+순서를 바꾸지않고 적절히 더하거나 빼서 타겟넘버
+1,1,1,1,1로 숫자 3만들기 -> 5가지 방법
+
+'''
 from collections import deque
 def solution(numbers, target):
-    queue = deque()
-    queue.append([numbers[0],0])
-    queue.append([-numbers[0],0])
-    cnt = 0
-    while queue :
-        v,i = queue.popleft()
-        if i==len(numbers)-1 and v == target:
-            cnt +=1
-        elif i < len(numbers)-1:
-            queue.append([v+numbers[i+1],i+1])
-            queue.append([v-numbers[i+1],i+1])
-
-    return cnt
+    q = deque()
+    q.append((0,numbers[0]))
+    q.append((0,-numbers[0]))
+    answer = 0
+    while q:
+        i,sum_n = q.popleft()
+        if sum_n == target and i==len(numbers)-1:
+            answer += 1
+        if i<len(numbers)-1:
+            plus=sum_n+numbers[i+1]
+            minus=sum_n-numbers[i+1]
+            q.append((i+1,plus))
+            q.append((i+1,minus))
+        
+    return answer
